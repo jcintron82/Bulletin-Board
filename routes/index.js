d87a9d8a6b4162924cbeb0558395ea02f79bec43
jcintron82 <71289948+jcintron82@users.noreply.github.com>
2022-12-18 17:36:33 -0500

    Solved bug in pug file causing the JSON output to output in an odd format and not on seperate lines

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,35 @@ var router = express.Router();
 
 
 const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
 ];
 
 
+const formattedSubtitle =  JSON.stringify(messages,null, 2).replaceAll('"','')
+.replaceAll('[','').replaceAll('{','').replaceAll('}','').replaceAll("]",'');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome', subtitle: JSON.stringify(messages)});
+  console.log('k')
+  res.render('index', { title: 'Welcome', subtitle: messages});
 });
 
+
+/* GET form page. */
 router.get('/new', function(req, res, next) {
   const title = 'Welcome' + '!';
   res.render('form', { title: title, subtitle: 'Test Subject...' });
 });
-//Left off here
+// Post new message to homepage 
 router.post('/new',(req, res) => {
   const name = req.body.name;
   const message = req.body.message;

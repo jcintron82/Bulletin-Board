3558c0a07fe5dbecde919943e862e16ac5344b8e
jcintron82 <71289948+jcintron82@users.noreply.github.com>
2022-12-19 23:34:21 -0500

    Fixed text overflowing issues on main page posts. Changed font style and button styling on home page. Ran code through prettier and a css formatter

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,37 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [];
 
-const messages = [
-
-];
-
-
-const formattedSubtitle =  JSON.stringify(messages,null, 2).replaceAll('"','')
-.replaceAll('[','').replaceAll('{','').replaceAll('}','').replaceAll("]",'');
+const formattedSubtitle = JSON.stringify(messages, null, 2)
+  .replaceAll('"', "")
+  .replaceAll("[", "")
+  .replaceAll("{", "")
+  .replaceAll("}", "")
+  .replaceAll("]", "");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log('k')
-  res.render('index', { title: 'Bulletin Board', subtitle: messages});
+router.get("/", function (req, res, next) {
+  console.log("k");
+  res.render("index", { title: "Bulletin Board", subtitle: messages });
 });
-
 
 /* GET form page. */
-router.get('/new', function(req, res, next) {
-  const title = 'Welcome' + '!';
-  res.render('form', { title: title, subtitle: 'Test Subject...' });
+router.get("/new", function (req, res, next) {
+  const title = "Welcome" + "!";
+  res.render("form", { title: title, subtitle: "Test Subject..." });
 });
-// Post new message to homepage 
-router.post('/new',(req, res) => {
+// Post new message to homepage
+router.post("/new", (req, res) => {
   const name = req.body.name;
   const message = req.body.message;
-  messages.push({user:name, text:message, added:new Date().toDateString()})
+  messages.push({
+    user: name,
+    text: message,
+    added: new Date().toDateString(),
+  });
 
-  res.redirect('/')
-})
+  res.redirect("/");
+});
 // messages.splice(0)
 module.exports = router;
